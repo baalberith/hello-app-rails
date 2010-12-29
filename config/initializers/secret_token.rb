a5529f01d9476a4883c28d5b1fdd09b83b80cee3
Baalberith <spr@spr-laptop.(none)>
2010-12-29 13:31:53 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PapaBear::Application.config.secret_token = '99950a4ee8daab23d544d34234899298b5c71816883f4a5de8517f2d70b13884c080e78c4c3f62783fe008e9b1ee3c40e5ca24a60df7985f2a32353394be985e'
